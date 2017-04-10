cce4d8f11d6c6d275d03bc23974541f49fd9c5b3
Denispok <vfkmxbr23@yandex.ru>
2017-04-10 18:11:29 +0700

    Добавил DB и DBHelper, который копирует существующую БД в приложение (пока что без самой БД).

--- app/src/main/java/com/gamesbars/whatyouchoose/LevelActivity.java
+++ app/src/main/java/com/gamesbars/whatyouchoose/LevelActivity.java
@@ -1,14 +1,32 @@
 package com.gamesbars.whatyouchoose;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.SQLException;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import static com.gamesbars.whatyouchoose.MainActivity.APP_PREFERENCES;
+
 public class LevelActivity extends AppCompatActivity {
+
+    SharedPreferences mSettings;
+    SharedPreferences.Editor editor;
+    DataBaseHelper myDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level);
 
+        mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+
+        // Открываем DataBase
+        myDbHelper = new DataBaseHelper(this);
+        try {
+            myDbHelper.openDataBase();
+        }catch(SQLException sqle){
+            throw sqle;
+        }
     }
 }
--- app/src/main/java/com/gamesbars/whatyouchoose/MainActivity.java
+++ app/src/main/java/com/gamesbars/whatyouchoose/MainActivity.java
@@ -1,12 +1,31 @@
 package com.gamesbars.whatyouchoose;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.SQLException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    //  Для работы с SQLite
+    public static final String TABLE_QUESTIONS_NAME = "QUESTIONS";
+    public static final String KEY_QUESTION_ONE = "QUEST1";
+    public static final String KEY_QUESTION_TWO = "QUEST2";
+    public static final String KEY_QUESTION_ONE_PERCENTAGE = "QUEST1PER";
+    public static final String KEY_QUESTION_TWO_PERCENTAGE = "QUEST2PER";
+    DataBaseHelper myDbHelper;
+
+    //  Для работы с Shared Preferences
+    public static final String APP_PREFERENCES = "settings";
+    public static final String APP_PREFERENCES_LVL = "level";
+    public SharedPreferences mSettings;
+    public SharedPreferences.Editor editor;
 
     Intent playIntent;
 
@@ -15,10 +34,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        playIntent = new Intent(this, LevelActivity.class);
+        //  Загружаем настройки или создаем новые при первом запуске
+        mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+        if (!mSettings.contains(APP_PREFERENCES_LVL)){
+            editor = mSettings.edit();
+            editor.putInt(APP_PREFERENCES_LVL,1);
+            editor.commit();
+        }
+
+        //  Создаем DataBase с помощью DataBaseHelper
+        myDbHelper = new DataBaseHelper(this);
+
+        try {
+            myDbHelper.createDataBase();
+        } catch (IOException ioe) {
+            throw new Error("Unable to create database");
+        }
     }
 
     public void play(View view){
+        playIntent = new Intent(this, LevelActivity.class);
         startActivity(playIntent);
     }
 }
